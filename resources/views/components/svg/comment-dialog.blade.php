058d21023d40a88d60ad1d7fda361ff21b8d81bd
221110019 <221110019@students.mikroskil.ac.id>
2025-12-08 07:53:46 +0700

    Add SVG Assets

---
+++ resources/views/components/svg/comment-dialog.blade.php
@@ -0,0 +1,17 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+>
+    <path
+        d="M4 3 h16 a2 2 0 0 1 2 2 v12 a2 2 0 0 1 -2 2 h-12 l-4 4 V5 a2 2 0 0 1 2 -2 z"
+    />
+    <path d="M8 8 h10" />
+    <path d="M8 12 h8" />
+    <path d="M8 16 h6" />
+</svg>
---
+++ resources/views/components/svg/image-plus.blade.php
@@ -0,0 +1,20 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="w-5 h-5 {{ $class ?? '' }}"
+>
+    <path d="M21 15V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h7" />
+    <circle
+        cx="14"
+        cy="12"
+        r="2"
+    />
+    <path d="M8 16l2-2 2 2 4-4" />
+    <path d="M22 19h-6" />
+    <path d="M19 16v6" />
+</svg>
