b8106c990b1108418700f6bb2a4c38fd3d9f6dac
221110019 <221110019@students.mikroskil.ac.id>
2026-01-10 04:26:46 +0700

    Remove unused filed & config Vite for Laragon

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    server: {
+        host: "catcanine-full.test",
+        hmr: {
+            host: "catcanine-full.test",
+        },
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
