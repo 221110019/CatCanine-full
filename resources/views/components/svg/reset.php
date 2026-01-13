6ece056c96f12675530792cbc365f5d28c36430b
221110019 <221110019@students.mikroskil.ac.id>
2026-01-14 05:44:11 +0700

    Add SVG

---
+++ resources/views/components/svg/reset.php
@@ -0,0 +1,18 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    class="w-6 h-6 text-success rounded-full p-1">
+    <path
+        d="M6 8a7 7 0 1 1-1 7"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round" />
+    <polyline
+        points="6 3 6 8 11 8"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round" />
+</svg>
